64a1c817f9b747eb10a092031c2503a4e30fb282
Beatriz Sampaio Medina <bee.a2@hotmail.com>
2021-10-15 20:02:02 -0300

    Atribuição dos valores de saque e deposito]

--- ConsoleMyBankApp/Program.cs
+++ ConsoleMyBankApp/Program.cs
@@ -11,11 +11,21 @@ namespace ConsoleMyBankApp
             ContaCorrente conta = new ContaCorrente(cliente, 01, 0001);
 
             Console.WriteLine("Conta Corrente criada!");
-            Console.WriteLine("Titular: {0}", conta.getTitular().getNome();
+            Console.WriteLine("Titular: {0}", conta.getTitular().getNome());
             Console.WriteLine("Agência: {0}", conta.getAgencia());
             Console.WriteLine("Conta: {0}", conta.getConta());
 
-            Console.WriteLine("Parabéns! Seu saldo é de R$ {0}, obrigado por criar sua conta!", conta.getSaldo());
+            Console.WriteLine("Parabéns! Seu saldo inicial é de R$ {0}, obrigado por criar sua conta!", conta.getSaldo());
+
+            double valorDeposito = 200;
+            conta.Depositar(valorDeposito);
+
+            Console.WriteLine("Seu saldo atual é R${0}.", conta.getSaldo());
+
+            double valorSaque = 400;
+            conta.Sacar(valorSaque);
+
+            //Console.WriteLine("Seu saldo atual é R${0}.", conta.getSaldo());
         }
     }
 }
