1a31429a9b26fb1cd465249c88e51832099c2609
Beatriz Sampaio Medina <bee.a2@hotmail.com>
2021-10-15 18:31:05 -0300

    criacao class conta corrente

--- ConsoleMyBankApp/Program.cs
+++ ConsoleMyBankApp/Program.cs
@@ -6,7 +6,22 @@ namespace ConsoleMyBankApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cliente cliente = new Cliente();
+
+            cliente.nome = "Beatriz Medina";
+            cliente.cpf = "0000000000000";
+            cliente.profissao = "Desenvolvedora";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = cliente;
+            conta.agencia = 01;
+            conta.conta = 0001;
+
+            Console.WriteLine("Conta Corrente criada!");
+            Console.WriteLine("Titular: {0}", conta.titular.nome);
+            Console.WriteLine("Agência: {0}", conta.agencia);
+            Console.WriteLine("Conta: {0}", conta.conta);
         }
     }
 }
