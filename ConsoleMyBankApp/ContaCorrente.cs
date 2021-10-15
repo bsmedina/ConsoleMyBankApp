30af83772ad891f3cf4fcb482d470158c6e79a12
Beatriz Sampaio Medina <bee.a2@hotmail.com>
2021-10-15 19:15:29 -0300

    .

--- ConsoleMyBankApp/ContaCorrente.cs
+++ ConsoleMyBankApp/ContaCorrente.cs
@@ -13,5 +13,48 @@ namespace ConsoleMyBankApp
         public int conta;
 
         private double saldo = 100.00;
+
+        public ContaCorrente(Cliente titular, int agencia, int conta)
+        {
+            this.setTitular(titular);
+            this.setAgencia(agencia);
+            this.setConta(conta);
+        }
+
+        public Cliente getTitular()
+        {
+            return this.titular;
+        }
+
+        public void setTitular(Cliente titular)
+        {
+           this.titular = titular;
+        }
+
+        public int getAgencia()
+        {
+            return this.agencia;
+        }
+
+        public void setAgencia(int agencia)
+        {
+           this.agencia = agencia;
+        }
+
+        public int getConta()
+        {
+            return this.conta;
+        }
+
+        public void setConta(int conta)
+        {
+           this.conta = conta;
+        }
+
+        public double getSaldo()
+        {
+            return this.saldo;
+        }
+
     }
 }
--- ConsoleMyBankApp/Program.cs
+++ ConsoleMyBankApp/Program.cs
@@ -6,22 +6,16 @@ namespace ConsoleMyBankApp
     {
         static void Main(string[] args)
         {
-            Cliente cliente = new Cliente();
+            Cliente cliente = new Cliente("Beatriz Medina", "000.000.000-00", "Dev");
 
-            cliente.nome = "Beatriz Medina";
-            cliente.cpf = "0000000000000";
-            cliente.profissao = "Desenvolvedora";
-
-            ContaCorrente conta = new ContaCorrente();
-
-            conta.titular = cliente;
-            conta.agencia = 01;
-            conta.conta = 0001;
+            ContaCorrente conta = new ContaCorrente(cliente, 01, 0001);
 
             Console.WriteLine("Conta Corrente criada!");
-            Console.WriteLine("Titular: {0}", conta.titular.nome);
-            Console.WriteLine("Agência: {0}", conta.agencia);
-            Console.WriteLine("Conta: {0}", conta.conta);
+            Console.WriteLine("Titular: {0}", conta.getTitular().getNome();
+            Console.WriteLine("Agência: {0}", conta.getAgencia());
+            Console.WriteLine("Conta: {0}", conta.getConta());
+
+            Console.WriteLine("Parabéns! Seu saldo é de R$ {0}, obrigado por criar sua conta!", conta.getSaldo());
         }
     }
 }
